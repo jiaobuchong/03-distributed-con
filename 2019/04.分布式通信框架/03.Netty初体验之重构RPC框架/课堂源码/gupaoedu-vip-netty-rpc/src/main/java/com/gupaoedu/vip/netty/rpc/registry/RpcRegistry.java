d12f677096f498b6e6ff5e107c66967676acb7fe
choujack <ijiaobu@qq.com>
2021-07-25 20:25:11 +0800

    ddd

--- 2019/04.分布式通信框架/03.Netty初体验之重构RPC框架/课堂源码/gupaoedu-vip-netty-rpc/src/main/java/com/gupaoedu/vip/netty/rpc/registry/RpcRegistry.java
+++ 2019/04.分布式通信框架/03.Netty初体验之重构RPC框架/课堂源码/gupaoedu-vip-netty-rpc/src/main/java/com/gupaoedu/vip/netty/rpc/registry/RpcRegistry.java
@@ -1,11 +1,7 @@
 package com.gupaoedu.vip.netty.rpc.registry;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.ChannelPipeline;
-import io.netty.channel.EventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
@@ -15,23 +11,32 @@ import io.netty.handler.codec.serialization.ClassResolvers;
 import io.netty.handler.codec.serialization.ObjectDecoder;
 import io.netty.handler.codec.serialization.ObjectEncoder;
 
-public class RpcRegistry {  
-    private int port;  
-    public RpcRegistry(int port){  
-        this.port = port;  
-    }  
-    public void start(){  
-        EventLoopGroup bossGroup = new NioEventLoopGroup();  
-        EventLoopGroup workerGroup = new NioEventLoopGroup();  
-          
-        try {  
+public class RpcRegistry {
+    private int port;
+
+    public RpcRegistry(int port) {
+        this.port = port;
+    }
+
+    public void start() {
+        // 主线程池 Selector
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        // 子线程初始化，处理对应客户端 Socket 的逻辑
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
             ServerBootstrap b = new ServerBootstrap();
+            // Selector 主线程，Work 线程
             b.group(bossGroup, workerGroup)
-            		.channel(NioServerSocketChannel.class)  
+                    .channel(NioServerSocketChannel.class)
                     .childHandler(new ChannelInitializer<SocketChannel>() {
-  
-                        @Override  
-                        protected void initChannel(SocketChannel ch) throws Exception {  
+
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            // 在 Netty 中，把所有的业务逻辑处理都归总到了一个队列中
+                            // 这个队列包含了各种各样的逻辑，对这些处理逻辑在 Netty 中有一个封装
+                            // 封装成了一个对象，无锁串行化队列
+                            // Pipeline
                             ChannelPipeline pipeline = ch.pipeline();
                             //自定义协议解码器
                             /** 入参有5个，分别解释如下
@@ -41,29 +46,41 @@ public class RpcRegistry {
                              lengthAdjustment：要添加到长度字段值的补偿值
                              initialBytesToStrip：从解码帧中去除的第一个字节数
                              */
-                            pipeline.addLast(new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 0, 4, 0, 4));
+                            // 解码
+                            pipeline.addLast(new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE,
+                                    0,
+                                    4,
+                                    0,
+                                    4));
                             //自定义协议编码器
                             pipeline.addLast(new LengthFieldPrepender(4));
                             //对象参数类型编码器
-                            pipeline.addLast("encoder",new ObjectEncoder());
+                            pipeline.addLast("encoder", new ObjectEncoder());
                             //对象参数类型解码器
-                            pipeline.addLast("decoder",new ObjectDecoder(Integer.MAX_VALUE,ClassResolvers.cacheDisabled(null)));
+                            pipeline.addLast("decoder", new ObjectDecoder(Integer.MAX_VALUE,
+                                    ClassResolvers.cacheDisabled(null)));
+
+                            // 前面的编解码对数据的解析
+                            // 最后一步，执行属于自己的逻辑
+
+                            // 1、注册，给每一个对象起一个名字，对外提供服务的名字
+                            // 2、服务位置做一个登记
                             pipeline.addLast(new RegistryHandler());
-                        }  
+                        }
                     })
-                    .option(ChannelOption.SO_BACKLOG, 128)       
-                    .childOption(ChannelOption.SO_KEEPALIVE, true);  
-            ChannelFuture future = b.bind(port).sync();      
-            System.out.println("GP RPC Registry start listen at " + port );
-            future.channel().closeFuture().sync();    
-        } catch (Exception e) {  
-             bossGroup.shutdownGracefully();    
-             workerGroup.shutdownGracefully();  
-        }  
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+            ChannelFuture future = b.bind(port).sync();
+            System.out.println("GP RPC Registry start listen at " + port);
+            future.channel().closeFuture().sync();
+        } catch (Exception e) {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
     }
-    
-    
-    public static void main(String[] args) throws Exception {    
-        new RpcRegistry(8080).start();    
-    }    
+
+
+    public static void main(String[] args) throws Exception {
+        new RpcRegistry(8080).start();
+    }
 }  
